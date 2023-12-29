1cf957568556a0d9fe87198ad2643635f0f8cfb6
Mason Galusha <masonlgalusha@GMAIL.COM>
2023-12-28 20:19:36 -0500

    Delete im_trying_blue_sky_settings.py

--- problems/im_trying_blue_sky_settings.py
+++
@@ -1,8 +0,0 @@
-class Settings:
-    # A class that stores the settings for the game
-
-    def __init__(self):
-        # Initialize game settings
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (58, 204, 249)
