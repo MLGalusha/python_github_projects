9c861ae3d6c6140ccfff4f365d40bf36c793a7bd
Mason Galusha <masonlgalusha@GMAIL.COM>
2023-12-29 16:28:49 -0500

    Update rocket.py

--- problems/rocket.py
+++ problems/rocket.py
@@ -14,8 +14,11 @@ class RocketGame:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        
         pygame.display.set_caption("Mr. Rocket")
 
         self.rocket_ship = RocketShip(self)
@@ -23,19 +26,47 @@ class RocketGame:
     def run_game(self):
         while True:
             self._check_events()
+            self.rocket_ship.update()
             self._update_events()
             self.clock.tick(60)
-
-    def _check_events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-   
+    
     def _update_events(self):
         self.screen.fill(self.settings.bg_color)
         self.rocket_ship.blitme()
 
         pygame.display.flip()
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.rocket_ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.rocket_ship.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.rocket_ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.rocket_ship.moving_down = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.rocket_ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.rocket_ship.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.rocket_ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.rocket_ship.moving_down = False
+ 
 
 if __name__ == "__main__":
     ai = RocketGame()
