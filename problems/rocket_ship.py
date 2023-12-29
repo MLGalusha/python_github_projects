b173e7f4595a1995aabe1dc5f879305dbe171c3f
Mason Galusha <masonlgalusha@GMAIL.COM>
2023-12-29 13:40:07 -0500

    Create rocket_ship.py

---
+++ problems/rocket_ship.py
@@ -0,0 +1,15 @@
+import pygame
+
+class RocketShip:
+
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        self.image = pygame.image.load("/Users/masongalusha/Desktop/python_work/good_spaceship_alien_invasion.png")
+        self.rect = self.image.get_rect()
+
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
