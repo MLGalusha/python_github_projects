444cbb3544ecc65cc2a53a5033d2549bcc8a4e4c
Mason Galusha <masonlgalusha@GMAIL.COM>
2023-12-29 22:19:39 -0500

    Update bullet.py

--- alien_invasion/bullet.py
+++ alien_invasion/bullet.py
@@ -19,3 +19,14 @@ class Bullet(Sprite):
 
         # store bullets position as a float
         self.y = float(self.rect.y)
+
+    def update(self):
+        #Move the bullet up on the screen
+        #Update the exact position of the bullet
+        self.y -= self.settings.bullet_speed
+        #Update the rect position
+        self.rect.y = self.y
+
+    def draw_bullets(self):
+        #Draw the bullets to the screen
+        pygame.draw.rect(self.screen, self.color, self.rect)
