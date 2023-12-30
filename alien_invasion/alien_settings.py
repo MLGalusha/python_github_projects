4176c3ed03d135066b004d9cb28c06e4d3d72f01
Mason Galusha <masonlgalusha@GMAIL.COM>
2023-12-29 22:19:42 -0500

    Update alien_settings.py

--- alien_invasion/alien_settings.py
+++ alien_invasion/alien_settings.py
@@ -14,4 +14,5 @@ class Settings:
         self.bullet_speed = 2.0
         self.bullet_width = 3.0
         self.bullet_height = 15
-        self.bullet_color = (60, 100, 60)
+        self.bullet_color = (200, 0, 175)
+        self.bullets_allowed = 6
