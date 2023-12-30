ad46dfb1196b46dc0a17e8abb73cb32069503715
Mason Galusha <masonlgalusha@GMAIL.COM>
2023-12-29 22:19:23 -0500

    Create fighter_jet_settings.py

---
+++ problems/fighter_jet_settings.py
@@ -0,0 +1,10 @@
+
+class Settings:
+
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_colors = (50, 200, 255)
+
+        self.jet_speed = 5
+        
