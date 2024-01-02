81549b4715c804e644530c8550af2d48138d6c21
Mason Galusha <masonlgalusha@GMAIL.COM>
2024-01-01 20:44:56 -0500

    Learning Pygame

--- alien_invasion/alien.py
+++ alien_invasion/alien.py
@@ -25,7 +25,15 @@ class Alien(Sprite):
 
         #Store the aliens exact horizontal position
         self.x = float(self.rect.x)
+
+    def check_edges(self):
+        #Return true if alien is on edge of screen
+        screen_rect = self.screen.get_rect()
+        return (self.rect.right >= screen_rect.right) or self.rect.left <= 0
+
     def update(self):
-        #Move the alien to the right
-        self.x += self.settings.alien_speed
+        #Move the alien to the left or right
+        self.x += self.settings.alien_speed * self.settings.fleet_direction
         self.rect.x = self.x
+
+    
--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -62,6 +62,20 @@ class AlienInvasion:
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
 
+    def _check_fleet_edges(self):
+        #respond appropriatly if any aliens have reached the edge
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        # Drop the entire fleet and change its direction
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+    
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -75,15 +89,19 @@ class AlienInvasion:
 
     def _update_aliens(self):
         # Update the position of all of the aliens in the fleet
+        self._check_fleet_edges()
         self.aliens.update()
           
     def _update_bullets(self):
-
         self.bullets.update()
         # Get rid of bullets that have disappeared.
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)      
+
+        # Check for any bullets that hit aliens
+        # If so remove bullet and alien that collided
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
 
     def _update_screen(self):
         # Update images on the screen, and flip to the next screen
